06cbce59b9e440992d4f5e83e0f848afada32b2d
xin yan <seahxinyan8@gmail.com>
2026-01-25 22:49:12 +0800

    Updated readme and dependencies

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# forumdev
-forum app development
+# ForumDev
+Forum app development project
+
+This is a general forum app that allows users to create topics (much like sub-reddits) to engage in discussion with the forum's community. Creators of the topic is able to manage posts and comments on their topic, allowing them to delete posts and comments that they deem unsuitable for the topic.
+Users are also able to freely post on topics and comment under posts to exp to engage in discussions with the topic's community.
+
+The application is only tried and tested on local machines and is yet to be deployed on the web. As such, instructions will be provided to run this application on your local browser.
+
+## Getting Started
+
+### Running the app:
+1. Install [Docker Desktop](https://www.docker.com/products/docker-desktop/) on your machine
+2. [Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo#forking-a-repository) this repo.
+3. [Clone](https://docs.github.com/en/get-started/quickstart/fork-a-repo#cloning-your-forked-repository) **your** forked repo.
+4. Open your terminal and navigate to the directory containing your cloned project.
+5. Run the app in development mode by entering this command:
+
+```bash
+docker compose up --build
+```
+
+6. Open [http://localhost:5173](http://localhost:5173) to view it in the browser.
+7. You should see a page like this.
+   ![Basic Page](frontend/public/Homepage.png)
+   
+## Acknowledgements
+
+This project was bootstrapped with [Create Vite](https://github.com/vitejs/vite/tree/main/packages/create-vite).
+This project utilised AI (in vscode) for its stylings
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,8 @@
 import TopicPage from "./pages/Topic/TopicPage";
 import TopicPostsView from "./pages/Topic/TopicPostsView";
 import PostPage from "./pages/Post/PostPage";
-// import CreateCommentPage from "./pages/Post/CreateCommentPage";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { blue, orange } from "@mui/material/colors";
 import Home from "./pages/Post/Home";
 import LoginView from "./pages/User/LoginView";
 import RegisterView from "./pages/User/RegisterView";
@@ -15,37 +12,24 @@ import UserProfile from "./pages/User/UserProfile";
 import Topics from "./pages/Topic/Topics";
 import Layout from "./pages/Layout";
 
-const theme = createTheme({
-    palette: {
-        primary: blue,
-        secondary: orange,
-    },
-    typography: {
-        fontFamily: "Poppins, sans-serif",
-    },
-});
-
 const App: React.FC = () => {
     return (
         <div className="App">
-            <ThemeProvider theme={theme}>
-                <BrowserRouter>
-                    <Routes>
-                        <Route element={<Layout />} >
-                            <Route path="/register" element={<RegisterView />} />
-                            <Route path="/login" element={<LoginView />} />
-                            <Route path="/" element={<Home />} />
-                            <Route path="/users" element={<Users />} />
-                            <Route path="/users/:user_id" element={<UserProfile />} />
-                            <Route path="/topics" element={<Topics />}/>
-                            <Route path="/topics/:topic_id" element={<TopicPage />} />
-                            <Route path="/topics/:topic_id/posts" element={<TopicPostsView />} />
-                            <Route path="/topics/:topic_id/posts/:post_id" element={<PostPage />} />
-                            {/* <Route path="/topics/:topic_id/posts/:post_id/comments" element={<CreateCommentPage />} /> */}
-                        </Route>
-                    </Routes>
-                </BrowserRouter>
-            </ThemeProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<Layout />} >
+                        <Route path="/register" element={<RegisterView />} />
+                        <Route path="/login" element={<LoginView />} />
+                        <Route path="/" element={<Home />} />
+                        <Route path="/users" element={<Users />} />
+                        <Route path="/users/:user_id" element={<UserProfile />} />
+                        <Route path="/topics" element={<Topics />}/>
+                        <Route path="/topics/:topic_id" element={<TopicPage />} />
+                        <Route path="/topics/:topic_id/posts" element={<TopicPostsView />} />
+                        <Route path="/topics/:topic_id/posts/:post_id" element={<PostPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 };
